19deafb784e69f05cebf2a15ad0ec0a8f4046205
Aparna Aidith <aparna.aidith@utexas.edu>
2019-09-05 13:27:46 -0700

    Enhanced for loop
    Enhanced for loop is useful when you want to iterate Array/Collections, it is easy to write and understand.

---
+++ Main (4).java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int arr[] = {2,4,8,10,12,14,20};
+	    for(int a:arr)
+	    {
+	        System.out.print(a+" ");
+	    }
+		
+	}
+}
