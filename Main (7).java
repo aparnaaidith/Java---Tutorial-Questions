3ad9f22abf4c72932f2f0d157361a1f61c1f3700
Aparna Aidith <aparna.aidith@utexas.edu>
2019-09-05 17:03:21 -0700

    Iterating an array using while loop

---
+++ Main (7).java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	   int arr[] = {2,4,8,10,12,14},i=1;
+	   while(i<=4)
+	   {
+	       System.out.print(arr[i]+" ");
+	       i++;
+	   }
+	   
+	}
+	
+}
